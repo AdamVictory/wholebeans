620d0944a66f3b5cd1cc198d8667b396d9a7eb34
AdamVictory <adam.victory1998@gmail.com>
2023-01-15 17:04:53 +0000

    build the models

--- coffee/models.py
+++ coffee/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class WholebeanPost(models.Model):
+    wholebean_id = models.SmallAutoField(primary_key=True)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, 
+        related_name='post_wholebean')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    wholebean_name = models.CharField(max_length=25)
+    wholebean_origin = models.CharField(max_length=25)
+    wholebean_brand = models.CharField(max_length=25)
+    wholebean_content = models.TextField(max_length=400, null=True)
+    wholebean_image = CloudinaryField('image', default='placeholder')
+    slug = models.Slugfield(unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta: 
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return self.wholebean_name
+
+
+    def save(save, *args, **kwargs):
+        """
+        Function to auto generate slugfield
+        """
+        autoslug = self.wholebean_brand + wholebean_name
+        self.slug = slugify(autoslug)
+        super(WholebeanPost, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(WholebeanPost,on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta: 
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
+
+
+
+   
