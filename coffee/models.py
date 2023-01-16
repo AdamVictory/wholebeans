3c70a8d4e1cf0db58fcb4271e9858abf9b4a860e
AdamVictory <adam.victory1998@gmail.com>
2023-01-16 15:02:18 +0000

    f

--- coffee/models.py
+++ coffee/models.py
@@ -1,46 +1,59 @@
+"""MODELS"""
+
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Added"))
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+class Recipe(models.Model):
+    """Recipe Model"""
+    recipe_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
+        User, on_delete=models.CASCADE, 
+        related_name="recipes")
+    added_on = models.DateTimeField(auto_now=True)
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    ingredients = models.TextField()
+    method = models.TextField()
+    recipe_image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+        User, related_name="recipe_likes", 
+        blank=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-added_on']
 
     def __str__(self):
-        return self.title
+        return self.recipe_name
 
     def number_of_likes(self):
         return self.likes.count()
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.recipe_name)
+        super(Recipe, self).save(*args, **kwargs)
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    """Comments Model"""
+    recipe = models.ForeignKey(Recipe,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=50)
+    added_on = models.DateTimeField(auto_now=True)
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User,related_name="comment_likes",blank=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ['added_on']
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+    def number_of_likes(self):
+        return self.likes.count()
