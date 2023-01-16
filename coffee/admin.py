ca7b47f24e79233768e147bf0d4daca981551243
AdamVictory <adam.victory1998@gmail.com>
2023-01-16 16:29:22 +0000

    admin.py

--- coffee/admin.py
+++ coffee/admin.py
@@ -6,4 +6,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('recipe_name',)}
+    list_filter = ('status', 'added_on', 'updated_on', 'likes')
+    list_display = ('recipe_name', 'added_on', 'status', 'updated_on')
+    search_fields = ('recipe_name', 'description', 'ingredients', 'method')
+    summernote_fields = ('description', 'ingredients', 'method')
+    actions = ['approve_recipes']
+
