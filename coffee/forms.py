a1f6e42629efd027abcb5d36737f0ca7352812c9
AdamVictory <adam.victory1998@gmail.com>
2023-01-16 15:47:13 +0000

    forms.py

---
+++ coffee/forms.py
@@ -0,0 +1,43 @@
+from .models import Comment, Recipe 
+from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+from django.forms import HiddenInput
+
+
+class CommentForm(forms.ModelForm): 
+    """Comment Form"""
+    class Meta:
+        model = Comment
+        fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    """Recipe Form"""
+    class Meta:
+        model = Recipe
+        fields = (
+            'recipe_name',
+            'description',
+            'ingredients',
+            'method',
+            'recipe_image',
+        )
+        widgets = {
+            'description': SummernoteWidget(),
+            'ingredients': SummernoteWidget(),
+            'method': SummernoteWidget(),
+        }
+
+
+class ApproveForm(forms.ModelForm):
+    """Form to Approve Recipes"""
+    class Meta: 
+        model = Recipe
+        fields = (
+            'slug',
+            'status',
+        )
+        widgets = {
+            'slug': HiddenInput(),
+        }
+
