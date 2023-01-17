3bcd2f422d850457e0fb23f68d243c9dbb5c419b
AdamVictory <adam.victory1998@gmail.com>
2023-01-17 12:52:46 +0000

    modify urls.py

--- wholebeans/urls.py
+++ wholebeans/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path, include 
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('coffee.urls'), name='coffee_urls'),
-    
+    path('accounts/', include('allauth.urls')), 
 ]
+
+HANDLER404 = 'wholebeans.view.custom_404_error'
+HANDLER500 = 'wholebeans.views.custom_500_error'
