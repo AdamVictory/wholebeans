4e87198f7a04cbe9b9a7aa9b02ec7f7cb6e39cb1
AdamVictory <adam.victory1998@gmail.com>
2023-01-17 11:19:35 +0000

    create first view

--- coffee/views.py
+++ coffee/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views import generic 
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-added_on')[0:6]
+    template_name = 'index.html'
 
--- wholebeans/urls.py
+++ wholebeans/urls.py
@@ -1,9 +1,11 @@
 """url paths"""
 from django.urls import path
-from . import views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('coffee.urls'), name='coffee_urls'),
+
 ]
