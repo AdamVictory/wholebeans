672f5f9d1ad507560cf3d9fc97270821abfa90c3
AdamVictory <adam.victory1998@gmail.com>
2023-01-19 10:52:12 +0000

    add views.py

---
+++ wholebeans/views.py
@@ -0,0 +1,12 @@
+"""Views for Custom Error Pages"""
+from django.shortcuts import render 
+
+
+def custom_404_error(request, exception): 
+    """Custom 404 Error View"""
+    return render(request. '404.html') 
+
+
+def custom_500_error(request, exception): 
+    """Custom 500 Error View"""
+    return render(request. '500.html') 
