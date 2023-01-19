471e001031f09cedcc56959ecf6f4da546a3e1fb
AdamVictory <adam.victory1998@gmail.com>
2023-01-19 11:07:45 +0000

    start index.html

--- wholebeans/urls.py
+++ wholebeans/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('coffee.urls'), name='coffee_urls'),
+    path('', include('coffee.urls'), name='coffee-urls'),
     path('accounts/', include('allauth.urls')), 
 ]
 
